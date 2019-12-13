9e4b53e412a3ef7bcb7ee54f63965d07f8443570
Marc Stammerjohann <marcstammerjohann@web.de>
2019-12-13 09:19:25 +0100

    chore(): ng 8 update

--- integration/app/app-routing.module.ts
+++ integration/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  }
 ];
 
 @NgModule({
