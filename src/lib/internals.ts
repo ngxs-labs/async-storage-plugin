970cc5270f6e1bad4d353301a85b41aeaf2f7fe6
Marc Stammerjohann <marcstammerjohann@web.de>
2019-12-13 09:30:48 +0100

    feat(storage-key): map state token key

--- src/lib/internals.ts
+++ src/lib/internals.ts
@@ -31,6 +31,8 @@ function transformKeyOption(key: StorageKey): string[] {
   return key.map((token: string | StateClass) => {
     if (typeof token === 'string') {
       return token;
+    } else if (token instanceof StateToken) {
+      return token.getName();
     }
 
     const options = (token as any)[META_OPTIONS_KEY];
