9390d1b6d88abf0e1e3a88b0e88bf0a4fb00b871
Marc <marcstammerjohann@web.de>
2019-12-13 10:15:27 +0100

    Angular deps update (#131)
    * chore(deps): angular 8 update
    
    * chore(): ng 8 update
    
    * chore(deps): update deps
    
    * feat(ng): drop angular 5 support
    
    * feat(storage-key): StateToken as key option
    
    * feat(storage-key): map state token key

--- src/lib/internals.ts
+++ src/lib/internals.ts
@@ -1,5 +1,6 @@
-import { StateClass } from '@ngxs/store/internals';
 import { NgxsStoragePluginOptions } from './symbols';
+import { StateClass } from '@ngxs/store/internals';
+import { StateToken } from '@ngxs/store';
 /**
  * If the `key` option is not provided then the below constant
  * will be used as a default key
@@ -10,7 +11,11 @@ export const DEFAULT_STATE_KEY = '@@STATE';
  * Internal type definition for the `key` option provided
  * in the `forRoot` method when importing module
  */
-export type StorageKey = string | StateClass | (string | StateClass)[];
+export type StorageKey =
+  | string
+  | StateClass
+  | StateToken<any>
+  | (string | StateClass | StateToken<any>)[];
 
 /**
  * This key is used to retrieve static metadatas on state classes.
@@ -26,6 +31,8 @@ function transformKeyOption(key: StorageKey): string[] {
   return key.map((token: string | StateClass) => {
     if (typeof token === 'string') {
       return token;
+    } else if (token instanceof StateToken) {
+      return token.getName();
     }
 
     const options = (token as any)[META_OPTIONS_KEY];
